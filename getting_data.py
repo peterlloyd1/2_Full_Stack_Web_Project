0712e25251f97c3d3f666b61511d73564ae652c0
peterlloyd1 <peter@zudrit.com>
2021-08-23 14:09:25 +0300

    Trying to use google profiles
    Trying to go with the google profile route instead of storing cookies.

--- getting_data.py
+++ getting_data.py
@@ -1,4 +1,3 @@
-from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
@@ -10,73 +9,15 @@ import json
 import time
 import pathlib
 
+from selenium import webdriver
 
-with open('login_details.json') as login_details:
-    data = json.load(login_details)
+options = webdriver.ChromeOptions()
+# Path to your chrome profile
+options.add_argument(
+    "user-data-dir=D:\\Five-Coding-Projects\\2_Full_Stack_Web_Project\\selenium")
+# Kinda works, need to understand which chrome profile to use.
 
-scriptDirectory = pathlib.Path().absolute()
+w = webdriver.Chrome(
+    executable_path="chromedriver.exe", options=options)
 
-chrome_options = Options()
-
-chrome_options.add_argument("--user-data-dir=chrome-data")
-# chrome_options.add_argument("user-data-dir=C:\environments\selenium")
-
-driver = webdriver.Chrome("chromedriver.exe", options=chrome_options)
-driver.get("https://ui8.net")  # Already authenticated
-time.sleep(30)
-driver.quit()
-
-driver.refresh()
-driver.get("https://ui8.net")
-
-
-# def get_info_UI8():
-
-#     # * Logging in on UI8
-#     # region
-
-#     try:
-#         driver.get("https://ui8.net/")
-#         print("Trying to log in on UI8")
-
-#         driver.implicitly_wait(15)
-
-#         driver.find_element_by_xpath(
-#             "/html/body/div[1]/header/div[2]/div[2]/a").click()
-
-#         email_input = driver.find_element_by_id("login-email")
-#         password_input = driver.find_element_by_id("login-password")
-
-#         email_input.send_keys(data["login"])
-#         password_input.send_keys(data["pw"])
-
-#         driver.find_element_by_id("login-button").click()
-
-#         print("Logged in on UI8")
-
-#     except:
-#         print("Login failed")
-
-#     # TODO: Just navigate to "https://ui8.net/account/signin?from=_affiliate_sales" and the login ?
-#     # TODO: Better would be to maybe store my cookies and stay logged in?
-
-#     # endregion
-
-#     # driver.get("https://ui8.net/affiliate/sales")
-#     # TODO: Storing the sales data in a JSON
-#     # region
-#     # TODO: Somehow need to get to this url https://ui8.net/affiliate/sales
-
-#     # endregion
-
-# # storing the cookies
-# pickle.dump(driver.get_cookies(), open("cookies.pkl", "wb"))
-
-
-# # loading the stored cookies
-# cookies = pickle.load(open("cookies.pkl", "rb"))
-# for cookie in cookies:
-#     print("Adding ")
-#     # adding the cookies to the session through webdriver instance
-#     driver.add_cookie(cookie)
-# driver.get('https://ui8.net')
+w.get("https://ui8.net/category/3d-assets")
