3331e44c0a472b63e7325e7b6bb742698726f91c
peterlloyd1 <peter@zudrit.com>
2021-08-26 00:02:01 +0300

    Wip data blendermarket
    Work in progress of storing data from blendermarket.

--- getting_data.py
+++ getting_data.py
@@ -1,38 +1,29 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-from csv import DictReader
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 
-import selenium.webdriver
-import pickle
 import json
-import time
-import pathlib
 
+# importing passwords for UI8
 with open('login_details.json') as login_details:
     data = json.load(login_details)
 
+# path to your chrome profile ( Make sure you have logged into websites your accessing )
 options = webdriver.ChromeOptions()
-# Path to your chrome profile
 options.add_argument(
     "user-data-dir=D:\\Five-Coding-Projects\\2_Full_Stack_Web_Project\\selenium")
-# Kinda works, need to understand which chrome profile to use.
-# Selenium profile: D:\\Five-Coding-Projects\\2_Full_Stack_Web_Project\\selenium
-# Main profile : C:\\Users\\User\\AppData\\Local\\Google\\Chrome\\User Data
 
-# options.add_argument('--profile-directory=Default')
-
+# selecting chrome path and options
 driver = webdriver.Chrome(
     executable_path="chromedriver.exe", options=options)
 
 
+# * Logging in on UI8
 def get_info_UI8():
-
-    # * Logging in on UI8
-    # region
 
     try:
         driver.get("https://ui8.net/")
@@ -57,20 +48,39 @@ def get_info_UI8():
         print("Login failed")
 
 
-driver.get("https://blendermarket.com/creator/sales")
-
-driver.execute_script("window.open('');")
-driver.switch_to.window(driver.window_handles[1])
-
-get_info_UI8()
-driver.implicitly_wait(10)
+driver.get(
+    "https://blendermarket.com/creator/sales?start_date=2021-01-01&end_date=2021-08-25")
 
 
-driver.execute_script("window.open('');")
-driver.switch_to.window(driver.window_handles[2])
+# region Opening other sites
+# driver.execute_script("window.open('');")
+# driver.switch_to.window(driver.window_handles[1])
 
-driver.get("https://www.cgtrader.com/profile/sales#latest-sales")
+# get_info_UI8()
+# driver.implicitly_wait(10)
 
-driver.switch_to.window(driver.window_handles[1])
 
-driver.get("https://ui8.net/affiliate/sales")
+# driver.execute_script("window.open('');")
+# driver.switch_to.window(driver.window_handles[2])
+
+# driver.get("https://www.cgtrader.com/profile/sales#latest-sales")
+
+# driver.switch_to.window(driver.window_handles[1])
+
+# driver.get("https://ui8.net/affiliate/sales")
+
+# blendermarket = driver.window_handles[0]
+# ui8 = driver.window_handles[1]
+# cgtrader = driver.window_handles[2]
+
+# driver.switch_to.window(blendermarket)
+
+# endregion
+# TODO: Loop trough blendermarket data tables and store the information
+# TODO: Date, Product name, Amount before fees, Earnings after fees
+
+# Printing all the items purchased
+print(driver.find_element_by_xpath('//table/tbody').text)
+
+# returning the date of last product purchased
+print(driver.find_element_by_xpath('//table/tbody/tr[1]/td[1]').text)
