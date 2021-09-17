67c0be276f5dcb5800ce235f1ee6116490238811
peterlloyd1 <peter@zudrit.com>
2021-09-17 14:30:05 +0300

    Return user choosen month
    Return products that have been sold in the selected date from the user.

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -5,6 +5,7 @@ function App() {
 
   return (
     <div className="App">
+      {/* TODO: Create a compontent to where we send a date and in return we recive data for that month */}
       <SalesSummery />
 
     </div >
--- admin-panel/src/components/SalesSummery.jsx
+++ admin-panel/src/components/SalesSummery.jsx
@@ -1,24 +1,50 @@
 import React from 'react'
-import BlenderMarketData from "../sales_data/blendermarket_sales.json"
+import UI8 from "../sales_data/ui8_sales.json"
+
 
 export default function SalesSummery() {
-    let revenue = 0
-    let asking_price = 0
+    // Need transfor my weird date to a normal date
+    var startDate = new Date("Mon Jun 1 2021 00:00:00 GMT+0300 (Eastern European Summer Time)");
+    var endDate = new Date("Mon Jun 30 2021 00:00:00 GMT+0300 (Eastern European Summer Time)");
 
-    BlenderMarketData.map((sale) => {
+    let ui8_revenue_product_01 = 0
+    let ui8_revenue_product_02 = 0
 
-        asking_price += parseInt(sale.asking_price.slice(1, -3))
-        revenue += parseInt(sale.revenue.slice(1, -3))
+    var result = UI8.filter(function (obj) {
+        console.log(obj.date);
+
+
+
+        // console.log("20" + parseInt(obj.date.slice(-2))) // Year
+        // console.log(parseInt(obj.date.slice(0, 2))) // Day
+        // console.log(parseInt(obj.date.slice(3, 5))) // Month
+
+        var date = new Date("20" + parseInt(obj.date.slice(-2)), parseInt(obj.date.slice(3, 5)) - 1, parseInt(obj.date.slice(0, 2)))
+
+        return date >= startDate && date <= endDate
+
+    })
+
+    console.log(result);
+
+    UI8.map((sale) => {
+
+        if (sale.product_name === "3D Character Illustration Pose Library Pack") {
+            ui8_revenue_product_01 += parseInt(sale.revenue.slice(1, -3))
+        }
+        else {
+            ui8_revenue_product_02 += parseInt(sale.revenue.slice(1, -3))
+
+        }
 
         return null;
     })
 
-
     return (
         <div>
-            <h1>Overall sales generate on BlenderMarket = {asking_price}</h1>
-            <p>After fees: {revenue}</p>
-        </div>
+            <h1>Product 1 generate {ui8_revenue_product_01}</h1>
+            <h1>Product 2 generate {ui8_revenue_product_02}</h1>
+        </div >
     )
 }
 
