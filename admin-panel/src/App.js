4d55461e4611a33c35cca4bd88df36f58a9ad018
peterlloyd1 <peter@zudrit.com>
2021-09-19 21:10:36 +0300

    got useState working
    use state works woaaa

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -1,12 +1,22 @@
 import "./app.css"
 import SalesSummery from "./components/SalesSummery"
+import Button from "@material-ui/core/Button"
+import { useState } from "react"
 
 function App() {
+
+  // var startDate = new Date("Mon Jul 1 2021 00:00:00 GMT+0300 (Eastern European Summer Time)");
+  // var endDate = new Date("Mon Jul 30 2021 00:00:00 GMT+0300 (Eastern European Summer Time)");
+
+  const [date, setDate] = useState({ "startDate": new Date("Mon Jul 1 2021 00:00:00 GMT+0300 (Eastern European Summer Time)"), "endDate": new Date("Mon Jul 30 2021 00:00:00 GMT+0300 (Eastern European Summer Time)") });
+
+
+
 
   return (
     <div className="App">
       {/* TODO: Create a compontent to where we send a date and in return we recive data for that month */}
-      <SalesSummery />
+      <SalesSummery startDate={date.startDate} endDate={date.endDate} />
 
     </div >
   );
--- admin-panel/src/components/SalesSummery.jsx
+++ admin-panel/src/components/SalesSummery.jsx
@@ -2,30 +2,30 @@ import React from 'react'
 import UI8 from "../sales_data/ui8_sales.json"
 
 
-export default function SalesSummery() {
-    // Need transfor my weird date to a normal date
-    var startDate = new Date("Mon Jun 1 2021 00:00:00 GMT+0300 (Eastern European Summer Time)");
-    var endDate = new Date("Mon Jun 30 2021 00:00:00 GMT+0300 (Eastern European Summer Time)");
+export default function SalesSummery(props) {
+
+    console.log(props.endDate);
+    // var startDate = new Date("Mon Jul 1 2021 00:00:00 GMT+0300 (Eastern European Summer Time)");
+    // var endDate = new Date("Mon Jul 30 2021 00:00:00 GMT+0300 (Eastern European Summer Time)");
 
     let ui8_revenue_product_01 = 0
     let ui8_revenue_product_02 = 0
 
     var result = UI8.filter(function (obj) {
-        console.log(obj.date);
-
-
-
+        // My date thingy is trash need to reforumalte to filter.
         // console.log("20" + parseInt(obj.date.slice(-2))) // Year
         // console.log(parseInt(obj.date.slice(0, 2))) // Day
         // console.log(parseInt(obj.date.slice(3, 5))) // Month
 
         var date = new Date("20" + parseInt(obj.date.slice(-2)), parseInt(obj.date.slice(3, 5)) - 1, parseInt(obj.date.slice(0, 2)))
 
-        return date >= startDate && date <= endDate
+        return date >= props.startDate && date <= props.endDate
 
     })
 
-    console.log(result);
+    result.map((sale) => {
+        console.log(sale);
+    })
 
     UI8.map((sale) => {
 
