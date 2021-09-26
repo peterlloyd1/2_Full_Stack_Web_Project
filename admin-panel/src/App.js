9bdc7ee89d63c2f2043ff87574a88e7be21bdc56
peterlloyd1 <peter@zudrit.com>
2021-09-26 22:35:44 +0200

    displaying information with state
    finally I can display info with state.

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -1,22 +1,42 @@
 import "./app.css"
 import SalesSummery from "./components/SalesSummery"
-import Button from "@material-ui/core/Button"
-import { useState } from "react"
+import { Button, InputLabel, MenuItem, FormHelperText, FormControl, Select } from "@material-ui/core"
+import React, { useState } from "react"
 
 function App() {
 
-  // var startDate = new Date("Mon Jul 1 2021 00:00:00 GMT+0300 (Eastern European Summer Time)");
-  // var endDate = new Date("Mon Jul 30 2021 00:00:00 GMT+0300 (Eastern European Summer Time)");
 
-  const [date, setDate] = useState({ "startDate": new Date("Mon Jul 1 2021 00:00:00 GMT+0300 (Eastern European Summer Time)"), "endDate": new Date("Mon Jul 30 2021 00:00:00 GMT+0300 (Eastern European Summer Time)") });
+  const [dates, setDates] = useState({ startDate: new Date("Mon Jul 1 2021"), endDate: new Date("Mon Jul 30 2021") })
+
+  const [state, setstate] = useState("halwa");
+
+  function changeContent(value) {
 
 
+    if (value === "Jun") {
+      setDates({ startDate: new Date("Mon Jun 1 2021"), endDate: new Date("Mon Jun 30 2021") })
+    } else if (value === "Jul") {
+      setDates({ startDate: new Date("Mon Jul 1 2021"), endDate: new Date("Mon Jul 30 2021") })
+    }
 
-
+  }
+  // Trying to chagne date based on event input so if its Jan, we call date and change it?
   return (
     <div className="App">
       {/* TODO: Create a compontent to where we send a date and in return we recive data for that month */}
-      <SalesSummery startDate={date.startDate} endDate={date.endDate} />
+
+
+      <div>
+        <Button onClick={() => changeContent("Jun")}>Jun</Button>
+        <Button onClick={() => changeContent("Jul")}>Jul</Button>
+        <h1>{dates.startDate.toString()}</h1>
+        <h1>{dates.endDate.toString()}</h1>
+
+      </div>
+
+      <SalesSummery startDate={dates.startDate} endDate={dates.endDate} />
+
+
 
     </div >
   );
--- admin-panel/src/components/SalesSummery.jsx
+++ admin-panel/src/components/SalesSummery.jsx
@@ -4,12 +4,9 @@ import UI8 from "../sales_data/ui8_sales.json"
 
 export default function SalesSummery(props) {
 
-    console.log(props.endDate);
-    // var startDate = new Date("Mon Jul 1 2021 00:00:00 GMT+0300 (Eastern European Summer Time)");
-    // var endDate = new Date("Mon Jul 30 2021 00:00:00 GMT+0300 (Eastern European Summer Time)");
-
     let ui8_revenue_product_01 = 0
     let ui8_revenue_product_02 = 0
+
 
     var result = UI8.filter(function (obj) {
         // My date thingy is trash need to reforumalte to filter.
@@ -23,27 +20,30 @@ export default function SalesSummery(props) {
 
     })
 
-    result.map((sale) => {
-        console.log(sale);
-    })
+    // UI8.map((sale) => {
 
-    UI8.map((sale) => {
+    //     if (sale.product_name === "3D Character Illustration Pose Library Pack") {
+    //         ui8_revenue_product_01 += parseInt(sale.revenue.slice(1, -3))
+    //     }
+    //     else {
+    //         ui8_revenue_product_02 += parseInt(sale.revenue.slice(1, -3))
 
-        if (sale.product_name === "3D Character Illustration Pose Library Pack") {
-            ui8_revenue_product_01 += parseInt(sale.revenue.slice(1, -3))
-        }
-        else {
-            ui8_revenue_product_02 += parseInt(sale.revenue.slice(1, -3))
+    //     }
 
-        }
-
-        return null;
-    })
+    //     return null;
+    // })
 
     return (
         <div>
-            <h1>Product 1 generate {ui8_revenue_product_01}</h1>
-            <h1>Product 2 generate {ui8_revenue_product_02}</h1>
+
+            {result.map((sale) => {
+                // console.log(sale.date);
+
+
+                return <p>{sale.sale}  {sale.date} {sale.product_name} {sale.revenue} </p>
+
+            })}
+
         </div >
     )
 }
